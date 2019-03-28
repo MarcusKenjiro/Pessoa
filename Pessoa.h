b2c327c0800d91989676876d9b75ae1731e447b7
marcus.kenjiro@gmail.com <kenji>
2019-03-27 21:26:44 -0300

    adicinado atributo cpf

--- Pessoa.h
+++ Pessoa.h
@@ -1,12 +1,17 @@
 #ifndef PESSOA_H
 #define PESSOA_H
 
+#include <string>
+using std::string;
+
 class Pessoa
 {
 public:
     Pessoa();
     ~Pessoa();
-
+private:
+    int cpf;
+    
 };
 
 #endif // PESSOA_H
